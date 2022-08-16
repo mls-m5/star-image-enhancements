3d7b09cf8f90371d4cbd0a8e21e041c983187402
Mattias Lasersköld <mattias.l.sk@gmail.com>
2022-08-16 17:52:16 +0200

    First successfull stacking

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,27 @@ ImgT transformImg(const ImgT &img, glm::mat4 mat) {
     return ret;
 }
 
+// TODO: Fix... Does not seem to work for whataever reason
+void applyTransformedImg(CImg<uint32_t> &target,
+                         const ImgT &source,
+                         glm::mat4 mat) {
+    auto inv = glm::inverse(mat);
+
+    cimg_forXY(target, x, y) {
+        auto c = glm::vec4{static_cast<float>(x), static_cast<float>(y), 0, 1};
+        auto c2 = inv * c;
+
+        cimg_for2C(target, color) {
+            uint32_t pixel = 0;
+            pixel = source.cubic_atXY_c(c2.x, c2.y, 0, color, pixel);
+            target(x, y, color) += pixel;
+            //            target(x, y, color) = pixel;
+        }
+    }
+
+    //    std::cout << target(10, 10, 0) << std::endl;
+}
+
 std::tuple<std::vector<ImgT>, ImgT> generateTestData(float rotation,
                                                      glm::vec2 translation) {
 
@@ -60,11 +81,15 @@ std::tuple<std::vector<ImgT>, ImgT> generateTestData(float rotation,
     return {imgs, colorImg};
 }
 
-void display(const ImgT &img1, const ImgT &colorImg) {
+void display(const ImgT &img1, const ImgT &colorImg, const ImgT &result) {
     auto row1 = ImgT{};
     row1.append(img1).append(colorImg);
 
+    auto row2 = ImgT{};
+    row2.append(result, 'x');
+
     auto disp = CImgDisplay{row1};
+    auto disp2 = CImgDisplay{row2};
     while (!disp.is_closed()) {
         disp.wait_all();
     }
@@ -106,6 +131,7 @@ void test1() {
     }
 
     auto displayTransform = glm::identity<glm::mat4>();
+    auto transforms = std::vector{glm::identity<glm::mat4>()};
 
     for (size_t i = 1; i < imgs.size(); ++i) {
         auto matches = getGetMatchingStars(imgs.at(i - 1), imgs.at(i));
@@ -114,18 +140,45 @@ void test1() {
         auto transform = findTransform(
             matches, {imgs.at(i).width() / 2, imgs.at(i).height() / 2});
 
+        // TODO: run find-transform from the first image to every following
+        // image
+
+        transforms.push_back(transform);
+
         displayTransform *= transform;
 
         drawMatrix(colorImg, displayTransform);
     }
 
-    display(imgs.at(1), colorImg);
+    auto result32 = CImg<uint32_t>{};
+    result32.resize(colorImg.width(), colorImg.height(), 3);
+
+    {
+        auto cumulativeTransform = glm::identity<glm::mat4>();
+
+        for (size_t i = 0; i < imgs.size(); ++i) {
+            cumulativeTransform *= transforms.at(i);
+            auto tmp =
+                transformImg(imgs.at(i), glm::inverse(cumulativeTransform));
+            result32 += tmp;
+        }
+    }
+
+    auto result = ImgT{};
+    result.resize(result32.width(), result32.height(), 3);
+    for (size_t i = 0; i < result.size(); ++i) {
+        result.at(i) = result32.at(i) / imgs.size();
+    }
+
+    display(imgs.at(1), colorImg, result);
 }
 
 void testDrawTransforms() {
     auto img = ImgT{};
 
     img.load_png("testdata/test-white.png");
+
+    //    img.resize(-100, -100, 1);
 
     auto mat = glm::identity<glm::mat4>();
 
@@ -149,7 +202,14 @@ void testDrawTransforms() {
 
 int main(int argc, char *argv[]) {
 
-    test1();
+    switch (0) {
+    case 0:
+        test1();
+        break;
+    case 1:
+        testDrawTransforms();
+        break;
+    }
 
     return 0;
 }
